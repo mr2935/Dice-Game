2010940d9c71ebc354e8e749b24b5912284b66db
mr2935 <103515186+mr2935@users.noreply.github.com>
2023-03-09 15:32:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomImageSource);
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomImageSource2="images/dice"+randomNumber2+".png";
+var randomImageSource2="dice"+randomNumber2+".png";
 var iamge2=document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 // if player 1 wins
 if(randomnumber1>randomNumber2){
